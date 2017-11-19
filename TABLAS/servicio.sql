b82e8343955e6b14dea1a040d8731ce7206fb4c8
Johann <johann.aevt@gmail.com>
2017-11-18 23:58:29 -0300

    FIXED: Error de nombres #2

--- TABLAS/servicio.sql
+++ TABLAS/servicio.sql
@@ -3,5 +3,5 @@ CREATE SEQUENCE sec_servicios;
 CREATE OR REPLACE TABLE Servicios (
     id_servicio INTEGER NOT NULL DEFAULT nextval('sec_servicios'),
     nombre_servicio VARCHAR(100) NOT NULL,
-    CONSTRAINT pk_paises PRIMARY KEY (id_servicio)
+    CONSTRAINT pk_servicios PRIMARY KEY (id_servicio)
 )
