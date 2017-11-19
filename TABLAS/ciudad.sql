3f30f186f338ac5bca10722b6742ed8c8864ff4f
Johann <johann.aevt@gmail.com>
2017-11-18 22:26:09 -0300

    Modificacion entidad Ciudades

--- TABLAS/ciudad.sql
+++ TABLAS/ciudad.sql
@@ -3,7 +3,7 @@ CREATE OR REPLACE SEQUENCE sec_ciudades;
 CREATE OR REPLACE TABLE Ciudades (
     id_ciudad INTEGER NOT NULL DEFAULT nextval('sec_ciudades'),
     nombre_ciudad VARCHAR(100),
-    id_pais INTEGER,
+    ciudad_pais INTEGER,
     CONSTRAINT pk_ciudades PRIMARY KEY (id_ciudad),
-    CONSTRAINT fk_ciudades_paises FOREIGN KEY (id_pais) REFERENCES paises(id_pais)
+    CONSTRAINT fk_ciudades_paises FOREIGN KEY (id_pais) REFERENCES Paises(id_pais)
 )
